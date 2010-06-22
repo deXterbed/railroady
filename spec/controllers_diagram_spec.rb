f272106c3546839b22181121805621b4fbe6b1d5
Dave Jones <unixmonkey1@gmail.com>
2010-06-23 03:15:56 +0800

    Add specs for controller globbing

---
+++ spec/controllers_diagram_spec.rb
@@ -0,0 +1,51 @@
+require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
+require 'controllers_diagram'
+require 'options_struct'
+
+describe ControllersDiagram do
+  describe 'file processing' do
+
+    it 'should select all the files under the controllers dir' do
+      cd = ControllersDiagram.new
+      files = cd.get_files("spec/file_fixture/")
+      files.size.should == 4
+    end
+
+    it 'should exclude a specific file' do
+      options = OptionsStruct.new(:exclude => ['spec/file_fixture/app/controllers/dummy1_controller.rb'])
+      cd = ControllersDiagram.new(options)
+      files = cd.get_files("spec/file_fixture/")
+      files.size.should == 3
+    end
+
+    it 'should exclude a glob pattern of files' do
+      options = OptionsStruct.new(:exclude => ['spec/file_fixture/app/controllers/sub-dir/*.rb'])
+      cd = ControllersDiagram.new(options)
+      files = cd.get_files("spec/file_fixture/")
+      files.size.should == 3
+    end
+
+    it 'should include only specific file' do
+      options = OptionsStruct.new(:specify => ['spec/file_fixture/app/controllers/sub-dir/sub_dummy_controller.rb'])
+      cd = ControllersDiagram.new(options)
+      files = cd.get_files("spec/file_fixture/")
+      files.size.should == 1
+    end
+
+    it 'should include only specified files' do
+      options = OptionsStruct.new(:specify => ['spec/file_fixture/app/controllers/{dummy1_*.rb,sub-dir/sub_dummy_controller.rb}'])
+      cd = ControllersDiagram.new(options)
+      files = cd.get_files("spec/file_fixture/")
+      files.size.should == 2
+    end
+
+    it 'should include only specified files and exclude specified files' do
+      options = OptionsStruct.new(:specify => ['spec/file_fixture/app/controllers/{dummy1_*.rb,sub-dir/sub_dummy_controller.rb}'],
+                                  :exclude => ['spec/file_fixture/app/controllers/sub-dir/sub_dummy_controller.rb'])
+      cd = ControllersDiagram.new(options)
+      files = cd.get_files("spec/file_fixture/")
+      files.size.should == 1
+    end
+
+  end
+end
