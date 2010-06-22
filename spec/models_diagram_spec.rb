ea94326fc1a54a63284e9e74922b61568cef99ef
Dave Jones <unixmonkey1@gmail.com>
2010-06-23 02:15:22 +0800

    Fix spacing/indentation of model_diagram spec

--- spec/models_diagram_spec.rb
+++ spec/models_diagram_spec.rb
@@ -4,24 +4,26 @@ require 'options_struct'
 
 describe ModelsDiagram do
   describe 'file  processing' do
+
     it 'should select all the files under the models dir' do
       md = ModelsDiagram.new
       files = md.get_files("spec/file_fixture/")
       files.size.should == 3
     end
-    
+
     it 'should exclude a specific file' do
       options = OptionsStruct.new(:exclude => ['spec/file_fixture/app/models/dummy1.rb'])
       md = ModelsDiagram.new(options)
       files = md.get_files("spec/file_fixture/")
       files.size.should == 2
     end
-     it 'should exclude a glob pattern of files' do
-        options = OptionsStruct.new(:exclude => ['spec/file_fixture/app/models/*/*.rb'])
-        md = ModelsDiagram.new(options)
-        files = md.get_files("spec/file_fixture/")
-        files.size.should == 2
-      end
-      
+
+    it 'should exclude a glob pattern of files' do
+      options = OptionsStruct.new(:exclude => ['spec/file_fixture/app/models/*/*.rb'])
+      md = ModelsDiagram.new(options)
+      files = md.get_files("spec/file_fixture/")
+      files.size.should == 2
+    end
+
   end
 end
